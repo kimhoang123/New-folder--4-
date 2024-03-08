6e54b2ba0163e801a070e2ff12525a3ec302b319
kimhoang123 <hoanggiathienkim7a5qtbl@gmail.com>
2024-03-08 21:25:47 +0700

    buoi3

---
+++ buoi3/loops.py
@@ -0,0 +1,30 @@
+#while
+#user_input = None
+#while(not user_input):
+#    user_input = input('Co thich minh khong=))?')
+#    if(user_input=="co"):
+#        break 
+ #   else:
+ #       user_input = None
+ #       continue
+#for
+#range
+#arr = range(1,10,3)
+#for i in arr :
+    #print(i)
+import time
+for i in "hahahahaah":
+    print(i)
+    time.sleep(0.5)#wait 0.5 sec
+print("heheheheheh")
+#while
+number_input = None
+while(not number_input):
+    num = input('nhap so:')
+      if(number_input.isdigit()):
+        if(int(num)%2==0)
+       break 
+    elif:
+        user_input = None
+        continue
+    else :
